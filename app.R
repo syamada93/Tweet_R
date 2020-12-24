6566ffd9927a0b7eec4fa316c7668feff64d40cd
syamada93 <Dendrobium93@gmail.com>
2020-12-24 16:07:39 +0900

    app

--- app.R
+++ app.R
@@ -65,21 +65,23 @@ library(rsconnect)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("ツイート数の推移"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+           textInput("wd",
+                     "抽出する単語",
+                     "雨"),
+           submitButton("抽出開始")
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("Hline"),
+           plotOutput("Dline"),
+           plotOutput("Mline"),
+           width = 12
         )
     )
 )
@@ -88,104 +90,187 @@ ui <- fluidPage(
 server <- function(input, output) {
     
 
-    refreshPlot <- reactiveTimer(intervalMs = 12000)
+    refreshPlot <- reactiveTimer(intervalMs = 60000)
 
     TDC <- data.frame()
     dc=""
     wd="雨"
     
-output$distPlot <-  renderPlot({
-        # # generate bins based on input$bins from ui.R
-        # x    <- faithful[, 2]
-        # bins <- seq(min(x), max(x), length.out = input$bins + 1)
-        # 
-        # # draw the histogram with the specified number of bins
-        # hist(x, breaks = bins, col = 'darkgray', border = 'white')
-        
+    WD <- reactive({
+        input$wd
+    })
     
-    
-        # repeat{
-        #     if(format(Sys.time(),"%S")=="00"){
+ observe({
+    wd=WD()
     refreshPlot()
-            td <- search_tweets(wd,lang = "ja",n = 1000,include_rts = T)
-            
-            if(nrow(td)!=0)
-            tds <-
-                td %>%
-                mutate(JTime=as.POSIXct(format(created_at, tz="Japan"))) %>%
-                mutate(Tweet=stri_trans_nfkc(text)) %>%
-                # mutate(Tweet=gsub("\"","",Tweet)) %>%
-                mutate(Tweet=gsub("http[[:print:]]{,18}","",Tweet)) %>%
-                mutate(Tweet=gsub("@[[:alnum:][:punct:]]+","",Tweet)) %>%
-                # mutate(Tweet=gsub("<U\\+0001","<U\\+1",Tweet)) %>%
-                mutate(Tweet=gsub("!+","!",Tweet)) %>%
-                mutate(Tweet=gsub("\\?+","\\?",Tweet)) %>%
-                mutate(Tweet=gsub("-+","-",Tweet)) %>%
-                mutate(Tweet=gsub("\\(+","\\(",Tweet)) %>%
-                mutate(Tweet=gsub(")+",")",Tweet)) %>%
-                mutate(Tweet=gsub("\"+","",Tweet)) %>%
-                mutate(Year=year(JTime)) %>%
-                mutate(Month=month(JTime)) %>%
-                mutate(Day=mday(JTime)) %>%
-                mutate(Hour=hour(JTime)) %>%
-                mutate(Minute=minute(JTime)) %>%
-                mutate(M=floor(Minute/10)*10) %>%
-                arrange(desc(status_id)) %>%
-                mutate(ID=paste0("Row",1:n()))
-            
-            day=format(max(tds$JTime),"%Y%m%d_%H%M") #-24*60*60
-            mid=max(tds$status_id)
-            print(paste(min(tds$JTime),max(tds$JTime),nrow(tds)))
-            
-            write_as_csv(tds,paste0("Tweet_data/Tweet_",wd,"_",day,"_",mid,".csv"),fileEncoding = "CP932")
-            
-            tdc <-
-                tds %>%
-                filter(!status_id %in% dc) %>%
-                count(Year,Month,Day,Hour,Minute,RT=is_retweet) %>%
-                mutate(RT=as.logical(RT)) %>%
-                complete(Year,Month,Day,Hour,Minute,RT,fill=list(n=0)) %>%
-                group_by(Year,Month,Day,Hour,Minute) %>%
-                mutate(total=sum(n)) %>%
-                ungroup() %>%
-                mutate(JTime=as.POSIXct(paste(Year,Month,Day,Hour,Minute),format="%Y %m %d %H %M"))
-            
-            TDC <-
-                TDC %>%
-                rbind(tdc) %>%
-                group_by(Year,Month,Day,Hour,Minute,RT) %>%
-                summarise(n=sum(n)) %>%
-                ungroup() %>%
-                complete(Year,Month,Day,Hour,Minute,RT,fill=list(n=0)) %>%
-                group_by(Year,Month,Day,Hour,Minute) %>%
-                mutate(total=sum(n)) %>%
-                ungroup() %>%
-                mutate(JTime=as.POSIXct(paste(Year,Month,Day,Hour,Minute),format="%Y %m %d %H %M"))
-            
-            dc <- sort(unique(c(tds$status_id,dc)),decreasing = T)
-            dc <- dc[1:min(length(dc),10000)]
-            
+    td <- search_tweets(wd,lang = "ja",n = 1000,include_rts = T)
+    
+    if(nrow(td)==0)
+        return()
+        
+    tds <-
+        td %>%
+        mutate(JTime=as.POSIXct(format(created_at, tz="Japan"))) %>%
+        mutate(Tweet=stri_trans_nfkc(text)) %>%
+        # mutate(Tweet=gsub("\"","",Tweet)) %>%
+        mutate(Tweet=gsub("http[[:print:]]{,18}","",Tweet)) %>%
+        mutate(Tweet=gsub("@[[:alnum:][:punct:]]+","",Tweet)) %>%
+        # mutate(Tweet=gsub("<U\\+0001","<U\\+1",Tweet)) %>%
+        mutate(Tweet=gsub("!+","!",Tweet)) %>%
+        mutate(Tweet=gsub("\\?+","\\?",Tweet)) %>%
+        mutate(Tweet=gsub("-+","-",Tweet)) %>%
+        mutate(Tweet=gsub("\\(+","\\(",Tweet)) %>%
+        mutate(Tweet=gsub(")+",")",Tweet)) %>%
+        mutate(Tweet=gsub("\"+","",Tweet)) %>%
+        mutate(Year=year(JTime)) %>%
+        mutate(Month=month(JTime)) %>%
+        mutate(Day=mday(JTime)) %>%
+        mutate(Hour=hour(JTime)) %>%
+        mutate(Minute=minute(JTime)) %>%
+        mutate(M=floor(Minute/10)*10) %>%
+        arrange(desc(status_id)) %>%
+        mutate(ID=paste0("Row",1:n()))
+    
+    day=format(max(tds$JTime),"%Y%m%d_%H%M") #-24*60*60
+    mid=max(tds$status_id)
+    print(paste(min(tds$JTime),max(tds$JTime),nrow(tds)))
+    
+    write_as_csv(tds,paste0("Tweet_data/Tweet_",wd,"_",day,"_",mid,".csv"),fileEncoding = "CP932")
+ 
+    tdc <-
+        tds %>%
+        filter(!status_id %in% dc) %>%
+        count(Year,Month,Day,Hour,Minute,RT=is_retweet) %>%
+        mutate(RT=as.logical(RT)) %>%
+        complete(Year,Month,Day,Hour,Minute,RT,fill=list(n=0)) %>%
+        group_by(Year,Month,Day,Hour,Minute) %>%
+        mutate(total=sum(n)) %>%
+        ungroup() %>%
+        mutate(JTime=as.POSIXct(paste(Year,Month,Day,Hour,Minute),format="%Y %m %d %H %M"))
+    
+    TDC <-
+        TDC %>%
+        rbind(tdc) %>%
+        group_by(Year,Month,Day,Hour,Minute,RT) %>%
+        summarise(n=sum(n)) %>%
+        ungroup() %>%
+        complete(Year,Month,Day,Hour,Minute,RT,fill=list(n=0)) %>%
+        group_by(Year,Month,Day,Hour,Minute) %>%
+        mutate(total=sum(n)) %>%
+        ungroup() %>%
+        mutate(JTime=as.POSIXct(paste(Year,Month,Day,Hour,Minute),format="%Y %m %d %H %M")) %>%
+        filter(JTime<Sys.time())
+    
+    dc <- sort(unique(c(tds$status_id,dc)),decreasing = T)
+    dc <- dc[1:min(length(dc),10000)]
+ 
+    output$Hline <-  renderPlot({
+        Comp <- 
+            # data.frame(JTime=(max(TDC$JTime)-60*60):(max(TDC$JTime))) %>%
+            data.frame(JTime=rep(seq(max(TDC$JTime)-60*60,max(TDC$JTime),60),each=2),
+                       RT=rep(c(F,T),61))
+        TDC2 <-
+            Comp %>%
+            left_join(TDC) %>%
+            mutate(n=ifelse(is.na(n),0,n))
+        
             p <-
-                TDC %>%
-                filter(total>0) %>%
-                # filter(JTime>"2020-11-05 15:30:00") %>%
+                TDC2 %>%
+                # filter(total>0) %>%
                 mutate(RTs=factor(RT,labels = c("オリジナルツイート","リツイート"))) %>%
                 ggplot(aes(x=JTime,y=n,fill=reorder(RTs,-RT))) +
                 geom_area(col="black") +
                 # geom_text(data=TDC2,aes(y=total+10,label=format(JTime,"%H"),fill=NULL),col="red") +
                 labs(x="",y="",fill="") +
-                scale_x_datetime(date_breaks="1 hours",date_labels = "%H時") +
-                scale_y_continuous(breaks = seq(0,10000000,100),limits = c(0,max(TDC$total+10))) +
-                # ggtitle("雨ツイート 更新中") +
+                scale_x_datetime(date_breaks="1 min",date_labels = "%H:%M") +
+                scale_y_continuous(breaks = seq(0,10000000,100),limits = c(0,max(TDC2$total+10))) +
+                ggtitle(paste0(min(TDC2$JTime),"～",max(TDC2$JTime))) +
                 theme(legend.position = "bottom") +
                 theme(text = element_text(size=30)) +
+                theme(axis.text.x =  element_text(size=20,angle = 90,vjust = 0.5)) +
                 theme(axis.title.x = element_blank())
             
             plot(p)
-            
-        #     }
-        # }
     })
+    
+    output$Dline <-  renderPlot({
+        TDCH <-
+            TDC %>%
+            group_by(Year,Month,Day,Hour,RT) %>%
+            summarise(n=sum(n)) %>%
+            ungroup() %>%
+            complete(Year,Month,Day,Hour,RT,fill=list(n=0)) %>%
+            group_by(Year,Month,Day,Hour) %>%
+            mutate(total=sum(n)) %>%
+            ungroup() %>%
+            mutate(JTime=as.POSIXct(paste(Year,Month,Day,Hour),format="%Y %m %d %H")) %>%
+            filter(JTime<Sys.time())
+        
+        Comp <- 
+            data.frame(JTime=rep(seq(max(TDCH$JTime)-24*60*60,max(TDCH$JTime),60*60),each=2),
+                       RT=rep(c(F,T),25))
+        TDC2 <-
+            Comp %>%
+            left_join(TDCH) %>%
+            mutate(n=ifelse(is.na(n),0,n))
+        
+        p <-
+            TDC2 %>%
+            # filter(total>0) %>%
+            mutate(RTs=factor(RT,labels = c("オリジナルツイート","リツイート"))) %>%
+            ggplot(aes(x=JTime,y=n,fill=reorder(RTs,-RT))) +
+            geom_area(col="black") +
+            # geom_text(data=TDC2,aes(y=total+10,label=format(JTime,"%H"),fill=NULL),col="red") +
+            labs(x="",y="",fill="") +
+            scale_x_datetime(date_breaks="1 hours",date_labels = "%H時") +
+            scale_y_continuous(breaks = seq(0,10000000,100),limits = c(0,max(TDC2$total+10))) +
+            ggtitle(paste0(min(TDC2$JTime),"～",max(TDC2$JTime))) +
+            theme(legend.position = "bottom") +
+            theme(text = element_text(size=30)) +
+            theme(axis.title.x = element_blank())
+        
+        plot(p)
+    })
+    
+    output$Mline <-  renderPlot({
+        TDCM <-
+            TDC %>%
+            group_by(Year,Month,Day,RT) %>%
+            summarise(n=sum(n)) %>%
+            ungroup() %>%
+            complete(Year,Month,Day,RT,fill=list(n=0)) %>%
+            group_by(Year,Month,Day) %>%
+            mutate(total=sum(n)) %>%
+            ungroup() %>%
+            mutate(JTime=as.POSIXct(paste(Year,Month,Day),format="%Y %m %d")) %>%
+            filter(JTime<Sys.time())
+        
+        Comp <- 
+            data.frame(JTime=rep(seq(max(TDCM$JTime)-31*24*60*60,max(TDCM$JTime),24*60*60),each=2),
+                       RT=rep(c(F,T),32))
+        TDC2 <-
+            Comp %>%
+            left_join(TDCM) %>%
+            mutate(n=ifelse(is.na(n),0,n))
+        
+        p <-
+            TDC2 %>%
+            # filter(total>0) %>%
+            mutate(RTs=factor(RT,labels = c("オリジナルツイート","リツイート"))) %>%
+            ggplot(aes(x=JTime,y=n,fill=reorder(RTs,-RT))) +
+            geom_area(col="black") +
+            # geom_text(data=TDC2,aes(y=total+10,label=format(JTime,"%H"),fill=NULL),col="red") +
+            labs(x="",y="",fill="") +
+            scale_x_datetime(date_breaks="1 days",date_labels = "%d日") +
+            scale_y_continuous(breaks = seq(0,10000000,100),limits = c(0,max(TDC2$total+10))) +
+            ggtitle(paste0(min(TDC2$JTime),"～",max(TDC2$JTime))) +
+            theme(legend.position = "bottom") +
+            theme(text = element_text(size=30)) +
+            theme(axis.title.x = element_blank())
+        
+        plot(p)
+    })
+ })
 }
 
 # Run the application 
